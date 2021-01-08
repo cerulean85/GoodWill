3115571b965838ffed70d7e37da06f6b6ea3f589
zhwan85@gmail.com <wlsghks3968>
2021-01-08 21:28:49 +0900

    refresh

---
+++ goodwillclient/src/test/java/com/kkennib/goodwill/GoodwillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkennib.goodwill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodwillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
